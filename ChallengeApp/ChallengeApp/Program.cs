f62e467ff1f1144d27502ba9f9df6b9ff6486639
Paweł Sulowski <sulopaulo@o2.pl>
2023-03-05 20:16:24 +0100

    Zadanie dzień czwarty.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+char sex = ';';  // 'M'- mężczyzna, 'W'- kobieta
+var age = 33;
+
+
+if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine(name + ",lat " + age);
+}
+if (sex == 'W' && age == 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat ");
+}
+if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("zmień wartości dla pozostałych");
+}
