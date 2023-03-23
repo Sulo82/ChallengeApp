a66dd5644f8e0964fc798a3e7d3e39c5e23ef8ee
Paweł Sulowski <sulopaulo@o2.pl>
2023-03-23 10:53:14 +0100

    Zadanie dzień czwarty (poprawione)

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,21 @@
 ﻿string name = "Ewa";
-char sex = ';';  // 'M'- mężczyzna, 'W'- kobieta
-var age = 33;
+char sex = 'M';  // 'M'- mężczyzna, 'W'- kobieta
+var age = 17;
 
 
 if (name == "Ewa" && age == 33)
 {
     Console.WriteLine(name + ",lat " + age);
 }
-if (sex == 'W' && age == 30)
+else if (sex == 'W' && age < 29)
 {
     Console.WriteLine("Kobieta poniżej 30 lat ");
 }
-if (sex == 'M' && age < 18)
+else if (sex == 'M' && age < 18)
 {
     Console.WriteLine("Niepełnoletni mężczyzna");
 }
 else
 {
-    Console.WriteLine("zmień wartości dla pozostałych");
+    Console.WriteLine("zmień wartości zmiennych");
 }
