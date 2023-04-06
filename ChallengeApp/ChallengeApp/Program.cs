c0d24acfc8170910edefd5f8ce6f4dca5eaf6a1b
Paweł Sulowski <sulopaulo@o2.pl>
2023-04-06 21:36:44 +0200

    Zadanie dzień piąty.

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,21 +1,70 @@
-﻿string name = "Ewa";
-char sex = 'M';  // 'M'- mężczyzna, 'W'- kobieta
-var age = 17;
+﻿var number = 2557779887;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
+Console.WriteLine("Ilość cyfr w liczbie " + numberAsString);
 
-if (name == "Ewa" && age == 33)
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 0;
+int counter8 = 0;
+int counter9 = 0;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine(name + ",lat " + age);
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '9')
+    {
+        counter9++;
+    }
 }
-else if (sex == 'W' && age < 29)
-{
-    Console.WriteLine("Kobieta poniżej 30 lat ");
-}
-else if (sex == 'M' && age < 18)
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
-else
-{
-    Console.WriteLine("zmień wartości zmiennych");
-}
+Console.WriteLine("Cyfr 0 jest " + counter0);
+Console.WriteLine("Cyfr 1 jest " + counter1);
+Console.WriteLine("Cyfr 2 jest " + counter2);
+Console.WriteLine("Cyfr 3 jest " + counter3);
+Console.WriteLine("Cyfr 4 jest " + counter4);
+Console.WriteLine("Cyfr 5 jest " + counter5);
+Console.WriteLine("Cyfr 6 jest " + counter6);
+Console.WriteLine("Cyfr 7 jest " + counter7);
+Console.WriteLine("Cyfr 8 jest " + counter8);
+Console.WriteLine("Cyfr 9 jest " + counter9);
